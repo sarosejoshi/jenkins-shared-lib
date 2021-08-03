cdf085e561ba23cec74c6e92ad801c10dcd2e1a6
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 <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>
2021-08-03 08:36:33 +0545

    aaded secret file

--- vars/Etl.groovy
+++ vars/Etl.groovy
@@ -219,7 +219,7 @@ stages {
                 '''
             sh '''
             cd config/
-               sed -i 's/\"image_tag\":.*/\"image_tag\":"$TAG_NAME"/g' "common/airflow/amazon_associate_etl_config.json"
+               sed -i 's/\"image_tag\":.*/\"image_tag\":"'$TAG_NAME'"/g' "common/airflow/amazon_associate_etl_config.json"
                scp -o StrictHostKeyChecking=no common/airflow/amazon_associate_etl_config.json ansible@ansible1.data.int.dc1.ad.net:/home/ansible/airflow/
                ssh -o StrictHostKeyChecking=no ansible@ansible1.data.int.dc1.ad.net docker cp /home/ansible/airflow/amazon_associate_etl_config.json eeb82e397165:/opt/airflow/dags
                ssh -o StrictHostKeyChecking=no ansible@ansible1.data.int.dc1.ad.net docker exec -i eeb82e397165 airflow variables import /opt/airflow/dags/amazon_associate_etl_config.json
@@ -243,8 +243,7 @@ stages {
           
             sh '''
                cd config/
-               export "image"="1.0.0"
-               sed -i 's/\"image_tag\":.*/\"image_tag\": "$image"/g' "common/airflow/amazon_associate_etl_config.json"
+               sed -i 's/\"image_tag\":.*/\"image_tag\": "latest"/g' "common/airflow/amazon_associate_etl_config.json"
                scp -o StrictHostKeyChecking=no common/airflow/amazon_associate_etl_config.json ansible@ansible1.data.int.dc1.ad.net:/home/ansible/airflow/
                ssh -o StrictHostKeyChecking=no ansible@ansible1.data.int.dc1.ad.net docker cp /home/ansible/airflow/amazon_associate_etl_config.json eeb82e397165:/opt/airflow/dags
                ssh -o StrictHostKeyChecking=no ansible@ansible1.data.int.dc1.ad.net docker exec -i eeb82e397165 airflow variables import /opt/airflow/dags/amazon_associate_etl_config.json
